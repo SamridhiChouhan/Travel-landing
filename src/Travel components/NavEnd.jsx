ac5c62518408022f93c097383622e55e736747e3
SamridhiChouhan <samridhichouhan1153@gmail.com>
2025-11-24 23:44:58 +0530

    useState for input

--- src/Travel components/NavEnd.jsx
+++ src/Travel components/NavEnd.jsx
@@ -1,17 +1,28 @@
 import "./NavEnd.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function NavEnd({ isOpen }) {
   let navInfo = {
     searchPlaceholder: "Search for a place, city or destination.",
     btn: "Book Now",
   };
+  let [search, setSearch] = useState("");
+
+  function changeValue(e) {
+    setSearch(e.target.value);
+  }
 
   return (
     <div className={isOpen ? "col-navEnd nav-end " : "nav-end"}>
       <form className="navSearch">
-        <input type="search" placeholder={navInfo.searchPlaceholder} />
+        <input
+          type="search"
+          placeholder={navInfo.searchPlaceholder}
+          value={search}
+          onChange={changeValue}
+        />
         <button>
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
